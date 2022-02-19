42f733413ecb58996b6d822c1e505b4b3dafc5ca
SimLej <simonlejoly@icloud.com>
2022-02-19 09:08:14 +0100

    changeDialog() function

--- dialog.js
+++ dialog.js
@@ -1,3 +1,13 @@
 /*
 Ici on code la boîte de dialogue et son affichage
 */
+
+function changeDialog(dialog) {
+    /*
+    Changes the dialog text
+    */
+    $("#dialog").text(dialog);
+}
+
+
+changeDialog("Salut!");
