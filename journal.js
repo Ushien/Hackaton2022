8b04ff1163d8ac2ef9c537156cc04852b228422d
Maragaux <100001703+Maragaux@users.noreply.github.com>
2022-02-20 00:19:13 +0100

    journal

--- journal.js
+++ journal.js
@@ -1,9 +1,12 @@
 let myMemories = {"flashback" : [], "happythought" : [], "learning" : [], "story" : []};
 
-function displayjournal(){
-    $(".modal-body").replaceWith('<div class="modal-body"><..></div>')
-    $("#staticBackdrop").modal("show")
+
+function displayJournal(){
+    console.log("ok")
+    $(".modal-body").replaceWith('<div class="modal-body"><.....></div>')
+    $("#staticBackdrop").modal("show");
 }
 
-let Journal_button = document.getElementById(journalButton)
-$("#journalButton").replaceWith('<button id="journalButton" class="upperButton" onClick="displayjournal()"><i class="bi-book" style="font-size: 3rem; color: white;"></i></button>')
+let Qbuttonjournal = document.getElementById(journalButton)
+$("#journalButton").replaceWith('<button id="journalButton" class="upperButton" onClick="displayJournal()"><i class="bi-book" style="font-size: 3rem; color: white;"></i></button>')
+
