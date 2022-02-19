77614fd6b2138ba0ec76b0a446d8a8cc669b0659
SimLej <simonlejoly@icloud.com>
2022-02-19 14:56:15 +0100

    Nouveau moteur de discussion

--- discussion.js
+++ discussion.js
@@ -2,47 +2,55 @@
 Ici on code la logique de discussion, en alternant les dialogs et les choices
 */
 
-let currentIndex = 0;
-let currentDiscussion = generateDiscussion("Salutations");
+let discution = ["Salutations"];
+let currentDialogSuiteIndex = 0;
+let currentDialogSuite = generateDialogSuiteFromName(discution[0]);
 
-buddySay(currentDiscussion[currentIndex])
+buddySay(Object.values(currentDialogSuite[currentDialogSuiteIndex])[0])
 
 function nextDialog() {
     // Goes to the next dialog
-    currentIndex++;
+    currentDialogSuiteIndex++;
 
-    console.log(currentIndex, currentDiscussion);
-    console.trace();
-
-    if (currentIndex > currentDiscussion.length) {
+    if (currentDialogSuiteIndex > currentDialogSuite.length) {
+        //TODO: passer à la dialogSuite suivante dans discution
         buddySay("<Rien de plus à dire>")
         return;
     }
 
-    if (currentIndex < currentDiscussion.length) {
-        if (! isChoice(currentDiscussion[currentIndex])) {
-            buddySay(currentDiscussion[currentIndex])
-        }
+    if (currentDialogSuiteIndex < currentDialogSuite.length) {
+        let dialogType = getDialogType(currentDialogSuite[currentDialogSuiteIndex]);
 
-        if (isChoice(currentDiscussion[currentIndex])) {
-            let choiceID = Object.keys(currentDiscussion[currentIndex])[0];
-            displayChoice(getChoice(choiceID));
+        console.log(dialogType);
+
+        switch (dialogType) {
+            case "d":
+                console.log(currentDialogSuite);
+                buddySay(Object.values(currentDialogSuite[currentDialogSuiteIndex])[0]);
+                break;
+            case "c":
+                let choiceID = Object.keys(currentDialogSuite[currentDialogSuiteIndex])[0];
+                displayChoice(getChoice(choiceID));
+                break;
+            case "m":
+                console.log("Modal!");
+                break;
         }
     }
 }
 
 function madeChoice(nb) {
     /* The user chose option n° nb */
-    let choiceID = Object.keys(currentDiscussion[currentIndex])[0];
-    currentDiscussion = generateDiscussionFromDialogList(currentDiscussion[currentIndex][choiceID][nb]);
-    currentIndex = -1;
+    let choiceID = Object.keys(currentDialogSuite[currentDialogSuiteIndex])[0];
+    currentDialogSuite = generateDialogSuiteFromDialogList(currentDialogSuite[currentDialogSuiteIndex][choiceID][nb]);
+    currentDialogSuiteIndex = -1;
     nextDialog();
     hideChoices();
 }
 
 
-function isChoice(dialog) {
-    return typeof dialog != "string";
+function getDialogType(dialog) {
+    return Object.keys(dialog)[0][0];
 }
 
 /*
@@ -50,28 +58,42 @@ Vous lui donnez un discussionTheme ("Salutations", "Au revoir")
 
 Il vous renvoie la chaîne de discussions par rapport à ces critères.
 */
-function generateDiscussion(dialogSuiteName){
+function generateDialogSuiteFromName(dialogSuiteName){
+    /*
+    Reçoit un nom de dialogSuite de dialog.js
+    Le converti en une suite de dialogues (choisit la version exacte de chaque dialogue)
+    Ex: [{"d001": "Bonjour!"}, {"d002": "Comment ça va ?"}, {"c001": {1: [...], 2: [...]}}]
+    */
     let possibleDialogs = dialogSuites[dialogSuiteName];
     let discussion = [];
 
     for (let dialogID of possibleDialogs) {
         let chosenDialog = dialogID;
-        if (! isChoice(dialogID)) {
-            chosenDialog = pickInList(getDialog(dialogID));
+        if (dialogID[0] == "d") {
+            chosenDialog = {};
+            chosenDialog[dialogID] = pickInList(getDialog(dialogID));
         }
         discussion.push(chosenDialog);
     }
 
+    console.log(discussion);
+
     return discussion;
 }
 
-function generateDiscussionFromDialogList(list) {
+function generateDialogSuiteFromDialogList(list) {
+    /*
+    Reçoit une liste d'ID de dialogues, choices, ...
+    Le converti en une suite de dialogues (choisit la version exacte de chaque dialogue)
+    Ex: [{"d001": "Bonjour!"}, {"d002": "Comment ça va ?"}, {"c001": {1: [...], 2: [...]}}]
+    */
     let discussion = [];
 
     for (let dialogID of list) {
         let chosenDialog = dialogID;
-        if (! isChoice(dialogID)) {
-            chosenDialog = pickInList(getDialog(dialogID));
+        if (dialogID[0] == "d") {
+            chosenDialog = {};
+            chosenDialog[dialogID] = pickInList(getDialog(dialogID));
         }
         discussion.push(chosenDialog);
     }
