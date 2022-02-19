1c4a82fa47461c85b710090398c9b512bb1f2cb3
Blandine <blandine.willemart@student.unamur.be>
2022-02-19 13:35:20 +0100

    en

--- dialog.js
+++ dialog.js
@@ -17,7 +17,7 @@ dialogs = {
         "d006" : ["Bonne nuit", "A bientôt", "Au revoir", "A demain", "Passez une bonne journée"]
     },
     "HT" : {
-        "d002" : ["Cela fait longtemps qu'on s'est pas vu", "Comment vas-tu ?", "Comment se passe ta journée ?", "Comment te sens-tu ?", "Est-ce que tu t'es senti en sécurité aujourd'hui ?", "Est-ce que tu t'es senti en seul ?", "As-tu eu de la visite récemment ?", "J'ai bien dormi et toi ?"],
+        "d002" : ["Cela fait longtemps qu'on s'est pas vu", "Comment vas-tu ?", "Comment se passe ta journée ?", "Comment te sens-tu ?", "Est-ce que tu t'es senti en sécurité aujourd'hui ?", "Est-ce que tu t'es senti seul ?", "As-tu eu de la visite récemment ?", "J'ai bien dormi et toi ?"],
         "d003" : ["Ajourd'hui tu pourrais faire " + activité, "Et si tu tentais" + activité+" ?", "Quelle belle journée pour faire " + activité],
         "d004" : ["As-tu fait une nouvelle activité aujourd'hui ?", "Désire-tu me partager un de tes souvenirs ?", "Qu'as-tu appris aujourd'hui ?","Quel a été ton meilleur moment de la journée", "En quoi cette journée a été spécial pour toi ?"],
         "d005" : ["Il y a " + temps + ", tu m'avais raconté que tu avais fait " + activité, "Quelle belle journée tu as eu !","Est-ce que tu te souviens de ce souvenir raconté il y a " + temps, "Telle personne a passé une journée similaire, voudrais-tu discuter avec elle"]
