e4b6fe639b25bc53c429e2ac3a0a7e7771ac1510
SimLej <simonlejoly@icloud.com>
2022-02-20 11:26:51 +0100

    Last modal

--- journal.js
+++ journal.js
@@ -2,10 +2,6 @@ let myMemories = [];
 
 
 function displayJournal(){
-    $(".modal-body").replaceWith('<div class="modal-body"><myMemories></div>')
-    $("#staticBackdrop").modal("show");
+    //TODO: remplir le journal
+    $("#modal6").modal("show");
 }
-
-let Qbuttonjournal = document.getElementById(journalButton)
-$("#journalButton").replaceWith('<button id="journalButton" class="upperButton" onClick="displayJournal()"><i class="bi-book" style="font-size: 3rem; color: white;"></i></button>')
-
