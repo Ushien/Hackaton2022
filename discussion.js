c6bd9450ce3f80c47e737663ee34a4105b7cdeca
Maragaux <100001703+Maragaux@users.noreply.github.com>
2022-02-20 09:35:51 +0100

    update dialogue

--- discussion.js
+++ discussion.js
@@ -2,7 +2,7 @@
 Ici on code la logique de discussion, en alternant les dialogs et les choices
 */
 let discutionPatterns = [
-    ["Check-up","Transition","Entree_souvenir","Transition","Histoire_commune","Au_revoir"],
+    ["Entree_souvenir","Transition","Histoire_commune","Au_revoir"],
     //["Salutations", "Salutations"],  ["Salutations","Premier_contact","Check-up","Transition",]
     //["Salutations", "Salutations", "Salutations"]
 ]
