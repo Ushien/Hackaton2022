a8ce5f67fb13fc448216ad7df201f536873fa5fa
SimLej <simonlejoly@icloud.com>
2022-02-20 10:50:38 +0100

    Bug fix

--- event.js
+++ event.js
@@ -4,18 +4,22 @@ Ici on définit les events (= du code à appeler pendant une discussion)
 
 function isMale() {
     userInfo.genre = "H";
+    console.log("h")
 }
 
 function isFemale() {
     userInfo.genre = "F";
+    console.log("f")
 }
 
 function tutoyer() {
     userInfo.preference = "T";
+    console.log("t")
 }
 
 function vouvoyer() {
     userInfo.preference = "V";
+    console.log("v")
 }
 
 let events = {
